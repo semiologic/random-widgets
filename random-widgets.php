1c52fbf3203c3e0c486199f98c861a2c328e9e00
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-06 16:57:30 +0000

    continuing on widget upgrade scripts
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/random-widgets/trunk@1974 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- random-widgets.php
+++ random-widgets.php
@@ -137,7 +137,7 @@ class random_widget extends WP_Widget {
 		if ( is_admin() ) {
 			echo $before_widget
 				. ( $title
-					? ( $before_title . apply_filters('widget_title', $title) . $after_title )
+					? ( $before_title . $title . $after_title )
 					: ''
 					)
 				. $after_widget;
@@ -790,24 +790,14 @@ class random_widget extends WP_Widget {
 			}
 		}
 		
-		# todo: switch
-		global $wp_filter, $_wp_sidebars_widgets;
-		$filter_backup = isset($wp_filter['sidebars_widgets']) ? $wp_filter['sidebars_widgets'] : array();
-		unset($wp_filter['sidebars_widgets']);
-		$_wp_sidebars_widgets = array();
-		$sidebars_widgets = wp_get_sidebars_widgets(false);
-		$wp_filter['sidebars_widgets'] = $filter_backup;
-		$_wp_sidebars_widgets = array();
-		
-		#global $_wp_sidebars_widgets;
-		#if ( is_admin() ) {
-		#	$sidebars_widgets = get_option('sidebars_widgets', array());
-		#} else {
-		#	if ( !$_wp_sidebars_widgets )
-		#		wp_get_sidebars_widgets(false);
-		#	$sidebars_widgets =& $_wp_sidebars_widgets;
-		#}
-		# /todo
+		global $_wp_sidebars_widgets;
+		if ( is_admin() ) {
+			$sidebars_widgets = get_option('sidebars_widgets', array('array_version' => 3));
+		} else {
+			if ( !$_wp_sidebars_widgets )
+				$_wp_sidebars_widgets = get_option('sidebars_widgets', array('array_version' => 3));
+			$sidebars_widgets =& $_wp_sidebars_widgets;
+		}
 		
 		$keys = array_keys($ops);
 		
@@ -823,11 +813,8 @@ class random_widget extends WP_Widget {
 			}
 		}
 		
-		# todo: switch
-		wp_set_sidebars_widgets($sidebars_widgets);
-		#if ( is_admin() )
-		#	update_option('sidebars_widgets', $sidebars_widgets);
-		# /todo
+		if ( is_admin() )
+			update_option('sidebars_widgets', $sidebars_widgets);
 		
 		return $ops;
 	} # upgrade()
