f32893710a0dce8971bd1b011dd5bdcbc13f3f99
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-12-12 18:48:28 +0000

    random widgets: Sem Cache 2.0 related enhancements
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/random-widgets/trunk@3705 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- random-widgets.php
+++ random-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: Random Widgets
 Plugin URI: http://www.semiologic.com/software/random-widgets/
 Description: WordPress widgets that let you list random posts, pages, links or comments.
-Version: 3.0.1
+Version: 3.0.2 alpha
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: random-widgets
@@ -720,12 +720,48 @@ class random_widget extends WP_Widget {
 	 **/
 
 	function save_post($post_id) {
+		if ( !get_transient('cached_section_ids') )
+			return;
+		
+		$post_id = (int) $post_id;
 		$post = get_post($post_id);
 		
 		if ( $post->post_type != 'page' )
 			return;
 		
-		delete_transient('cached_section_ids');
+		$section_id = get_post_meta($post_id, '_section_id', true);
+		$refresh = false;
+		if ( !$section_id ) {
+			$refresh = true;
+		} else {
+			_get_post_ancestors($post);
+			if ( !$post->ancestors ) {
+				if ( $section_id != $post_id )
+					$refresh = true;
+			} elseif ( $section_id != $post->ancestors[0] ) {
+				$refresh = true;
+			}
+		}
+		
+		if ( $refresh ) {
+			global $wpdb;
+			if ( !$post->post_parent )
+				$new_section_id = $post_id;
+			else
+				$new_section_id = get_post_meta($post->post_parent, '_section_id', true);
+			
+			if ( $new_section_id ) {
+				update_post_meta($post_id, '_section_id', $new_section_id);
+				wp_cache_delete($post_id, 'posts');
+				
+				# mass-process children
+				if ( $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_parent = $post_id AND post_type = 'page' LIMIT 1") )
+					delete_transient('cached_section_ids');
+			} else {
+				# fix corrupt data
+				delete_transient('cached_section_ids');
+			}
+		}
 	} # save_post()
 	
 	
@@ -755,7 +791,7 @@ class random_widget extends WP_Widget {
 		
 		foreach ( $pages as $page ) {
 			$parent = $page;
-			while ( $parent->post_parent )
+			while ( $parent->post_parent && $parent->ID != $parent->post_parent )
 				$parent = get_post($parent->post_parent);
 			
 			if ( "$parent->ID" !== get_post_meta($page->ID, '_section_id', true) )
