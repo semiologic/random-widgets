0187ba06bbfe9bc15f6dd47fec610545fc12d4fc
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-05-10 15:01:00 +0000

    sort out auto-draft related issues
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/random-widgets/trunk@4376 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- random-widgets.php
+++ random-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: Random Widgets
 Plugin URI: http://www.semiologic.com/software/random-widgets/
 Description: WordPress widgets that let you list random posts, pages, links or comments.
-Version: 3.0.3
+Version: 3.0.4 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: random-widgets
@@ -778,7 +778,7 @@ class random_widget extends WP_Widget {
 			SELECT	*
 			FROM	$wpdb->posts
 			WHERE	post_type = 'page'
-			AND		post_status <> 'trash'
+			AND		post_status IN ( 'publish', 'private' )
 			");
 		
 		update_post_cache($pages);
